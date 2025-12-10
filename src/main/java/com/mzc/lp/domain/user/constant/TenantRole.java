464f6543ffe34eae73e21d23a04e89e1be17ffe4
shin-mz <ggg9905@mz.co.kr>
2025-12-10 21:49:19 +0900

    [Fix] TenantRole, CourseRole 문서 기준으로 수정
    - TenantRole: TENANT_ADMIN, OPERATOR, USER 순서로 변경
    - CourseRole: 상세 설명 주석 추가

--- src/main/java/com/mzc/lp/domain/user/constant/TenantRole.java
+++ src/main/java/com/mzc/lp/domain/user/constant/TenantRole.java
@@ -1,7 +1,7 @@
 package com.mzc.lp.domain.user.constant;
 
 public enum TenantRole {
-    USER,           // 일반 사용자
-    OPERATOR,       // 운영자
-    TENANT_ADMIN    // 테넌트 관리자
+    TENANT_ADMIN,   // 테넌트 최고 관리자
+    OPERATOR,       // 운영자 (강의 검토, 차수 생성, 역할 부여)
+    USER            // 일반 사용자 (수강)
 }
