bfd140eb6b8d15c4982cfddb8a2f0c9e7d023afc
chan-mz <snoopy0813@mz.co.kr>
2026-01-04 21:01:10 +0900

    feat: UserDetailResponse에 courseRoles 필드 추가 (#266)
    - UserDetailResponse에 List<CourseRoleResponse> courseRoles 필드 추가
    - UserCourseRoleRepository에 Program title 조인 쿼리 추가
    - UserServiceImpl의 getMe(), getUser(), updateMe(), updateUser() 메서드에서 courseRoles 포함
    - courseRoles가 없는 사용자는 빈 배열 반환

--- src/main/java/com/mzc/lp/domain/user/repository/UserCourseRoleRepository.java
+++ src/main/java/com/mzc/lp/domain/user/repository/UserCourseRoleRepository.java
@@ -3,6 +3,8 @@ package com.mzc.lp.domain.user.repository;
 import com.mzc.lp.domain.user.constant.CourseRole;
 import com.mzc.lp.domain.user.entity.UserCourseRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +12,15 @@ import java.util.Optional;
 public interface UserCourseRoleRepository extends JpaRepository<UserCourseRole, Long> {
 
     List<UserCourseRole> findByUserId(Long userId);
+
+    /**
+     * 사용자의 CourseRole 목록을 Program title과 함께 조회
+     * courseId는 실제로 Program ID를 저장하므로 Program 테이블과 조인
+     */
+    @Query("SELECT ucr, p.title FROM UserCourseRole ucr " +
+           "LEFT JOIN Program p ON ucr.courseId = p.id " +
+           "WHERE ucr.user.id = :userId")
+    List<Object[]> findByUserIdWithProgramTitle(@Param("userId") Long userId);
 
     Optional<UserCourseRole> findByUserIdAndCourseIdIsNullAndRole(Long userId, CourseRole role);
 
